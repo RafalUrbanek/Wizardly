b5c1ce7c8c053d2f2ca2c6946291c96a0c94dd66
Rafal Urbanek <rafal.m.urbanek@gmail.com>
2020-05-13 23:41:04 +0100

    - semi-random room spawn on the map - stairs spawn in random room (except spawn room)

--- venvNew/DungeonGenerator.py
+++ venvNew/DungeonGenerator.py
@@ -11,6 +11,7 @@ class DungeonMap:
         self.spawn_y = spawn_y
         self.row = []
         self.map_matrix = []
+        self.stairs_pos = []
         self.initialize()
         self.generate()
 
@@ -21,6 +22,7 @@ class DungeonMap:
                 self.row.append(Cell(0, False))
             self.map_matrix.append(self.row)
             self.row = []
+        print("map initialized, size: rows " + str(len(self.map_matrix)) + "  lines " + str(len(self.map_matrix[0])))
 
     def percent_used(self):
         used = 0
@@ -34,7 +36,7 @@ class DungeonMap:
 
     def generate(self):
 
-        # generate spawn room around spawn point
+        # generate 3x3 spawn room around spawn point
         spawn_room = [[self.spawn_x - 1, self.spawn_y - 1],
                       [self.spawn_x, self.spawn_y - 1],
                       [self.spawn_x + 1, self.spawn_y - 1],
@@ -49,32 +51,47 @@ class DungeonMap:
             if 0 <= tile[0] <= self.width - 1:
                 if 0 <= tile[1] <= self.height - 1:
                     self.map_matrix[tile[1]][tile[0]] = Cell()
+        print("spawn room generated")
 
         # generate random rooms
         attempts = 100
         while attempts > 0 and self.percent_used() < 70:
             room_size = self.get_room_size()
-            room_spawn_x = random.randint(room_size + 1, self.width - room_size - 1)
-            room_spawn_y = random.randint(room_size + 1, self.width - room_size - 1)
+            room_spawn_x = random.randint(room_size // 2 + 1, self.width - room_size // 2 - 1)
+            room_spawn_y = random.randint(room_size // 2 + 1, self.height - room_size // 2 - 1)
 
             if self.test_room_position(room_spawn_x, room_spawn_y, room_size):
                 self.build_room(room_spawn_x, room_spawn_y, room_size)
-            print("  attempt " + str(attempts) + "  size: " + str(room_size) + "  spawn point: " + str(
-                room_spawn_x) + " " + str(room_spawn_y))
             attempts -= 1
+        print("rooms generated")
+
+        while True:
+            valid = True
+            position = (random.randint(0, self.width-4), random.randint(0, self.height-4))
+            for y in range(position[1], position[1] + 4):
+                for x in range(position[0], position[0] + 4):
+                    if self.map_matrix[y][x].tile_id == 0:
+                        valid = False
+                        break
+            if valid:
+                self.stairs_pos = [position[0] + 2, position[1] + 2]
+                self.map_matrix[self.stairs_pos[1]][self.stairs_pos[0]] = Cell(3)
+                break
 
     def test_room_position(self, room_spawn_x, room_spawn_y, room_size):
         valid = True
         for y in range(room_spawn_y - (room_size // 2) - 1, room_spawn_y + (room_size // 2) + 1):
             for x in range(room_spawn_x - (room_size // 2) - 1, room_spawn_x + (room_size // 2) + 1):
-                if self.map_matrix[x][y].tile_id != 0:
-                    valid = False
+                if x < len(self.map_matrix[0]) and y < (len(self.map_matrix)):
+                    if self.map_matrix[y][x].tile_id != 0:
+                        valid = False
+                        break
         return valid
 
     def build_room(self, room_spawn_x, room_spawn_y, room_size):
         for y in range(room_spawn_y - (room_size // 2), room_spawn_y + (room_size // 2)):
             for x in range(room_spawn_x - (room_size // 2), room_spawn_x + (room_size // 2)):
-                self.map_matrix[x][y] = Cell()
+                self.map_matrix[y][x] = Cell()
 
     def get_room_size(self):
         if self.width < 20 or self.height < 20:
@@ -95,13 +112,18 @@ class Cell:
 
 
 class PrintMap:
-    map = DungeonMap(30, 30, 1, 1)
+    """Temporary class for visualising generated data"""
+    map = DungeonMap(40, 19, 1, 1)
     for line in range(len(map.map_matrix)):
         for cell in range(len(map.map_matrix[line])):
-            if map.map_matrix[line][cell] != 0:
-                print(map.map_matrix[line][cell].tile_id, end='')
-            else:
-                print(map.map_matrix[line][cell], end='')
+            if map.map_matrix[line][cell].tile_id == 1:
+                print(" ", end='')
+            elif map.map_matrix[line][cell].tile_id == 0:
+                print(u'\u2588', end='')
+            elif map.map_matrix[line][cell].tile_id == 3:  # temporary symbol for stairs
+                print(u'\u2599', end='')
+            elif map.map_matrix[line][cell].tile_id == 2:  # temporary symbol for corridor
+                print(u'\u2591', end='')
         print()
 
 
